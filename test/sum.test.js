163b4bf2c2becfbeed71deaadd4df2f3c5970686
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2021-10-15 11:06:51 +0200

    multiply function and test

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,9 +1,21 @@
 const sum = require('../src/sum');
+const multiply = require('../src/multiply')
+
+function checkNumber(firstNum, secondNum) {
+    expect(typeof firstNum).toBe("number")   
+    expect(typeof secondNum).toBe("number")
+}
 
 test('adds 1 + 1 to equal 2', () => {  
     const firstNum = 1
     const secondNum = 1
-    expect(typeof firstNum).toBe("number")   
-    expect(typeof secondNum).toBe("number")   
+    checkNumber(firstNum, secondNum)
     expect(sum(firstNum, secondNum)).toBe(2);
 });
+
+test('multiplies 1 by 3 to equal 3', () => {
+    const firstNum = 1
+    const secondNum = 3
+    checkNumber(firstNum, secondNum)
+    expect(multiply(firstNum, secondNum)).toBe(2);
+})
